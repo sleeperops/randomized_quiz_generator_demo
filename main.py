26ed46418dd0d1f9a9dd1bb4a1b0ee23e5913ad4
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-11 22:02:36 +0800

    Refactor: Encapsulate code into functions

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
-# Directory of the file that stores all formatted question
-question_directory = r"C:\Users\Pau\Desktop\Computer Engineering notes and more\OOP_Materials\randomized_quiz_generator_demo\questions\questions"
+# Directory of the quiz file that stores all formatted questions
+quiz_directory = r"C:\Users\Pau\Desktop\Computer Engineering notes and more\OOP_Materials\randomized_quiz_generator_demo\questions\questions"
 
-# Runs a loop and accepts input until the user decides to exit
-while True: 
-
-    # Accepts the input for the question
-    input_question = input("Enter a question: ")
-    with open(question_directory, "a") as question_file:  # Writes the question into the file
-        question_file.write(f'''{input_question}
+def write_question(input_question, file_directory):
+    """
+    Accepts a question and writes it into a specified file
+    """
+    with open(file_directory, "a") as fd:  # Writes the question into the specified file
+        fd.write(f'''{input_question}
 ''')
-    print(input_question)
 
-    # Accepts the input for the options   
-    input_options = {"a":"", "b":"", "c":"", "d":""}
-    for char in input_options:  # Loops through the options and sets a value for each
-        input_options[char] = input(f"Enter option {char}: ")
+def write_option(file_directory):
+    """
+    Asks for and writes the options into a specified file
+    """
+    option_block = ""
+    for char in "abcd":  # Creates a block of string with that contains the options
+        option = input(f"Option {char}: ")
+        option_block += f'''{char}). {option}
+'''
+    with open(file_directory) as fd:  # Writes that block of string into the file
+        fd.write(option_block)
 
-    with open(question_directory, "a") as question_file:  # Writes the option into the file
-        question_file.write(f'''{input_options}
-''')
-    print(input_options)
-
-
-    # Accepts the input for the correct answer
-    input_answer = input("Enter the letter of the correct answer: ")
-    with open(question_directory, "a") as question_file:  # Writes the correct answer into the file
+def write_correct(input_answer, file_directory):
+    """
+    Accepts and writes the correct answer into the specified file
+    """
+    with open(file_directory, "a") as question_file:  # Writes the correct answer into the file
         question_file.write(f'''{input_answer}
 ''')
+    
+# Runs a loop; accepts and writes the inputs until the user decides to exit
+while True: 
+    # Accepts the input for the question
+    input_question = input("Enter a question: ")
+
+    # Writes the input into a specified quiz file
+    write_question(input_question,quiz_directory)
+
+    # Accepts the input for the options and writes it into the specified quiz file at the same time
+    write_option(quiz_directory)
+        
+    # Accepts the input for the correct answer
+    input_answer = input("Enter the letter of the correct answer: ")
+    
+    # Writes the input for the correct answer
+    write_correct(input_answer, quiz_directory)
 
     # Gives user the ability to exit or continue the loop
     user_option = input("Do you wish to continue? (any key to continue and // to exit): ")
     if user_option == "//":
         break
 
-print(input_question)
-print(input_options)
-print(input_answer)
