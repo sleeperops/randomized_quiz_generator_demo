881d87a13d38e8b461e3d313edbce87f12b7ffcd
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-04-24 18:48:13 +0800

    Add quiz_app.py and its pseudocode

---
+++ quiz_app.py
@@ -0,0 +1,10 @@
+# Initialize class item_question
+# Intitialize 'question', 'options', and 'correct' as properties of that class
+# Initialize 'question_list'
+# Read each line from 'quiz.txt' and group each question, options and correct answers
+#     into variables, then store them into 'question_list'.
+# Use random package to select a random question from the list.
+# Ask the user a question until they exit.
+# Count the number of times they generated a question 
+# Count the number of times they met the correct answer
+# Calculate the difference and display the result
