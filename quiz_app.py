ee07d13d1ba292142cea9ed84e51ed25124d800e
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-05-26 17:40:08 +0800

    Feat: Add a feature that allows the quiz_dictionary to be iterable

--- quiz_app.py
+++ quiz_app.py
@@ -1,4 +1,3 @@
-# Quiz files?
 # Check for spelling errors
 
 # Read quiz has to run first and set a value for line list before you can use extract and repack
@@ -18,16 +17,16 @@ class Item:
 
 def read_quiz(file_directory):
     """
-    Reads the quiz file and extracts each questions into a list
-    Accepts the file directory of the quiz file and returns a list with all the questions
+    Reads the quiz.txt file and extracts each questions into a list
+    Accepts the file directory of the quiz.txt and returns a list with all the questions
     """
     
-    # Stores each lines extracted from the quiz file
+    # Stores each lines extracted from the quiz.txt
     lines_list = []  
 
     # Use a context manager to open and automatically close the file once the lines have been extracted
     with open(file_directory, "r") as quiz:
-        temp_lines_list = quiz.readlines()  # Stores each lines of the quiz file to a temporary list before stripping      
+        temp_lines_list = quiz.readlines()  # Stores each lines of the quiz.txt file to a temporary list before stripping      
         
         # Loops through the entire list and removes the /n in each line that is in that list
         for line in temp_lines_list: 
@@ -49,6 +48,9 @@ def extract_and_repack(lines_list):
     # A list of objects (referred to as items) of the class 'Item'. The lines from line_list are passed as properties of these items
     quiz_dict = {}
 
+    # Assign each item a number as a form of identifier.
+    counter = 1
+
     # Loop through 'line_list' using the 'lines' variable to track the current line and 'index' to track its index
     for index, lines in enumerate(lines_list):  
         if str(lines).startswith("Q:") == True:  # If the line starts with the 'Q:' prefix, construct an instance of the class 'Item' with the following properties:
@@ -63,7 +65,10 @@ def extract_and_repack(lines_list):
             question_item = Item(question, option_a, option_b, option_c, option_d, correct_answer) 
 
             # Store each item in a dictionary using the property 'question' as the key and the item itself as the value
-            quiz_dict[question_item.question] = question_item 
+            quiz_dict[counter] = question_item 
+
+            # Increments by 1 for every loop
+            counter += 1
 
     return quiz_dict
 
@@ -77,4 +82,4 @@ print(f"""LINES LIST:
 print(f"""REPACKED LINES LIST:
        {quiz_items_list}
 """)
-
+print((quiz_items_list[1].option_a))
